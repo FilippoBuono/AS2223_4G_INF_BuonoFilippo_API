f7bb7e08c3ad524af5c8674dec3dc0017bb8d68f
FilippoBuono <101173382+FilippoBuono@users.noreply.github.com>
2023-03-16 08:19:06 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/MathController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2223_4G_INF_BuonoFilippo_API.Controllers
+{
+    [ApiController]
+    []
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
