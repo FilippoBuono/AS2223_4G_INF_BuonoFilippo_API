110a46cc8cdb054caf4af1ed9f3c34f0e655accf
FilippoBuono <101173382+FilippoBuono@users.noreply.github.com>
2023-03-16 08:47:55 +0100

    implementazione dei metodi per calcolare il fattoriale e per trovare se il numero è pari o dispari

--- Controllers/MathController.cs
+++ Controllers/MathController.cs
@@ -1,14 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace AS2223_4G_INF_BuonoFilippo_API.Controllers
 {
     [ApiController]
-    []
+    [Produces("application/Json")]
+    [Route("")]
     public class MathController : Controller
     {
+        public object valore { get; private set; }
+
         public IActionResult Index()
         {
             return View();
         }
+
+
+        [HttpGet ("PariDispariGet")]
+        public JsonContent PariDispariGet(int value)
+        {
+            string pariDis;
+            if (value % 2 == 1)
+            {
+                pariDis = "Il valore è dispari";
+            }
+            else { pariDis = "Il valore è pari"; }
+
+            return Json(new { output = "pariDis", value=value }); 
+        }
+        [HttpGet("FattorialeGet")]
+
+        public JsonContent FattorialeGet(int valueFatt)
+        {
+            int fatt = 1;
+            for(int i = 1; i <= valueFatt; i++)
+            {
+                fatt = fatt * i;
+            }
+            return Json(new { fattoriale = fatt });
+        }
+
     }
+
 }
